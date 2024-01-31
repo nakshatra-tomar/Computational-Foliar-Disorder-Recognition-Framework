7bf48c0019a0eb90cab866c9f700294c9251b039
Nakshatra Tomar <tomarnakshatra@gmail.com>
2024-01-31 23:46:20 +0530

    api01312

--- api/main.py
+++ api/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+import numpy
+from io import BytesIO
+from PIL import Image
 
 app = FastAPI()
+
 
 @app.get("/ping")
 async def ping():
     return "Test"
+
+def read_image(data)-> numpy.ndarray:       #data is bytes as input
+    image =numpy.array(Image.open(BytesIO(data))
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
+):
+    image = read_image(file.read())
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)

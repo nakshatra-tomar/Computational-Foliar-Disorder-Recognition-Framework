2b8c8533c073b8a0fb77b823f75f60bfead86ab6
Nakshatra Tomar <tomarnakshatra@gmail.com>
2024-02-02 13:29:37 +0530

    api02021

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
 import numpy
 from io import BytesIO
@@ -10,31 +10,42 @@ app = FastAPI()
 
 
 model = tf.keras.models.load_model("../Models/1")
-classes = ['Tomato_Bacterial_spot',
- 'Tomato_Early_blight',
- 'Tomato_Late_blight',
- 'Tomato_Leaf_Mold',
- 'Tomato_Septoria_leaf_spot',
- 'Tomato_Spider_mites_Two_spotted_spider_mite',
- 'Tomato__Target_Spot',
- 'Tomato__Tomato_YellowLeaf__Curl_Virus',
- 'Tomato__Tomato_mosaic_virus',
- 'Tomato_healthy']
+classes = ['Tomato Bacterial Spot',
+ 'Tomato Early blight',
+ 'Tomato Late blight',
+ 'Tomato Leaf Mold',
+ 'Tomato Septoria leaf spot',
+ 'Tomato Spider mites Two spotted spider mite',
+ 'Tomato Target Spot',
+ 'Tomato YellowLeaf Curl Virus',
+ 'Tomato Tomato mosaic virus',
+ 'Tomato healthy']
 @app.get("/ping")
 async def ping():
     return "Test"
 
 def read_image(data)-> numpy.ndarray:       #data is bytes as input
-    image =numpy.array(Image.open(BytesIO(data))
+    image =numpy.array(Image.open(BytesIO(data)))
+    return image
 @app.post("/predict")
 async def predict(
         file: UploadFile = File(...)
 ):
-    image = read_image(file.read())
+    image = read_image(await file.read())
 
-    img_batch = numpy.expand_dims()             #model.predict only accepts image batches, current image is[256,256,3] so converting to higher dimensional array [[256,256,3]]
+    img_batch = numpy.expand_dims(image, 0)             #model.predict only accepts image batches, current image is[256,256,3] so converting to higher dimensional array [[256,256,3]]
 
     prediction = model.predict(img_batch)
+
+    index = numpy.argmax(prediction[0])
+
+    predicted_class = classes[index]
+    confidence = numpy.max(prediction[0])
+
+    return{
+        'class' : predicted_class,
+        'confidence' : float(confidence)
+    }
 
 
 if __name__ == "__main__":
